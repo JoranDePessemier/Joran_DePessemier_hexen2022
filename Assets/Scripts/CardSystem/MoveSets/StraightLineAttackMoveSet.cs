6cd9dca4ac08bd3da0e1cdcee538759ded9b4656
JoranDePessemier <joran.de.pessemier@student.howest.be>
2023-01-15 12:36:44 +0100

    ADD Working card prefab

---
+++ Assets/Scripts/CardSystem/MoveSets/StraightLineAttackMoveSet.cs
@@ -0,0 +1,26 @@
+﻿using BoardSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardSystem.MoveSets
+{
+    class StraightLineAttackMoveSet : MoveSet
+    {
+        public StraightLineAttackMoveSet(Board board) : base(board)
+        {
+        }
+
+        public override bool Execute(Position fromPosition, Position toPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Position> Positions(Position fromPosition, Position hoverPosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

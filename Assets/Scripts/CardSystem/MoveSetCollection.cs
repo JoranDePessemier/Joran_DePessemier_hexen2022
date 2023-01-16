ce8d8077e7fbb49b0cf82fc639e70e0b183add57
JoranDePessemier <joran.de.pessemier@student.howest.be>
2023-01-16 08:56:19 +0100

    ADD Extra card to deck

--- Assets/Scripts/CardSystem/MoveSetCollection.cs
+++ Assets/Scripts/CardSystem/MoveSetCollection.cs
@@ -17,6 +17,7 @@ namespace CardSystem
             _moveSets.Add(CardType.Teleport, new TeleportMoveSet(board));
             _moveSets.Add(CardType.AdjacentAttack, new AdjacentAttackMoveSet(board));
             _moveSets.Add(CardType.PushEnemies, new PushEnemiesMoveSet(board));
+            _moveSets.Add(CardType.Meteor, new MeteorMoveSet(board));
         }
 
         //other classes can ask the moveset for a specific cardttype
---
+++ Assets/Scripts/CardSystem/MoveSets/MeteorMoveSet.cs
@@ -0,0 +1,26 @@
+﻿using BoardSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardSystem.MoveSets
+{
+    class MeteorMoveSet : MoveSet
+    {
+        public MeteorMoveSet(Board board) : base(board)
+        {
+        }
+
+        public override bool Execute(Position fromPosition, Position toPosition)
+        {
+            return true;
+        }
+
+        public override List<Position> Positions(Position fromPosition, Position hoverPosition)
+        {
+            return new List<Position>();
+        }
+    }
+}
